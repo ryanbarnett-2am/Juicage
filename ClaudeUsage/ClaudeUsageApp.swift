8e7e1e6d888bcf44ab28f79f7c02685f6f01da38
Ryan Barnett <ryan@2amideas.com>
2026-04-25 13:47:43 -0500

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Ryan Barnett on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
